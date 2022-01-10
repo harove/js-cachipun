507327a4395d6483427a76a09166c0e1be096e74
Edgar Alexander Haro Garcia <harove@gmail.com>
2022-01-10 00:26:25 -0500

    play again just when play again

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let login = false
+let playAgain = false
 
 function onLogin(){
     const user = document.getElementById("user-input").value
@@ -9,27 +10,34 @@ function onLogin(){
         console.log("isLogin")
         document.getElementById("play-container").classList.remove("none")
         document.getElementById("play-container").classList.add("flex")
+        playAgain=true
     }  
 }
 
 function onChooseMyPlay(selection){
-    const aiAlternatives = ['right-pp','right-st','right-sc']
-    const aiPlay = aiAlternatives[Math.floor(Math.random() * 3 )]
-    document.getElementById("my-selection").innerHTML = `<img src='./images/${selection}.jpg' alt='leftHandPaper' width='100px')'>`
-    document.getElementById("ai-selection").innerHTML = `<img src='./images/${aiPlay}.jpg' alt='leftHandPaper' width='100px')'>`
-    switch (key) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
+    if (playAgain===true){
+        const aiAlternatives = ['right-pp','right-st','right-sc']
+        const aiPlay = aiAlternatives[Math.floor(Math.random() * 3 )]
+        console.log(selection)
+        console.log(aiPlay)
+        document.getElementById("my-selection").innerHTML = `<img src='./images/${selection}.jpg' alt='leftHandPaper' width='100px')'>`
+        document.getElementById("ai-selection").innerHTML = `<img src='./images/${aiPlay}.jpg' alt='leftHandPaper' width='100px')'>`
+        switch (selection) {
+            case 'rigth-pp':
+                
+                break;
+        
+            default:
+                break;
+        }
+        playAgain=false
     }
 }
 
 function onPlayAgain(){
     document.getElementById("my-selection").innerHTML = `<div class="my-selection-inner">?</div>`
     document.getElementById("ai-selection").innerHTML = `<div class="ai-selection-inner">?</div>`
+    playAgain = true
 }
 
 
